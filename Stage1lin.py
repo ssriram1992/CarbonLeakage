8eba1a01e336c4ff1ee986ceaecf22e52e21fae3
Sriram Sankaranarayanan <srirams@iima.ac.in>
2022-04-16 19:37:02 +0530

    add makeOutputProtoType and writedata function

--- Stage1lin.py
+++ Stage1lin.py
@@ -4,6 +4,29 @@ import matplotlib.pyplot as plt
 from utils import *
 import Stage1 
 
+def makeOutputProtoTypes(xGridder, yGridder):
+    xGrid, yGrid = np.meshgrid(xGridder, yGridder)
+    Qrr = np.zeros_like(xGrid)
+    Qur = np.zeros_like(xGrid)
+    invUR = np.zeros_like(xGrid)
+    emission = np.zeros_like(xGrid)
+    profits = np.zeros_like(xGrid)
+    CLtype = np.zeros_like(xGrid)
+    invType = np.zeros_like(xGrid)
+    return xGrid, yGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType
+
+def writedata(xGrid, yGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType, folder = './data1/'):
+    np.savetxt(folder+"xx.csv",xGrid, delimiter=',')
+    np.savetxt(folder+"yy.csv",yGrid, delimiter=',')
+    plotIt(xGrid, yGrid, Qrr, title = "Exp Qty in RR", folder=folder)
+    plotIt(xGrid, yGrid, Qur, title = "Exp Qty in UR", folder=folder)
+    plotIt(xGrid, yGrid, invUR, title = "Capacity Inv in UR", folder=folder)
+    plotIt(xGrid, yGrid, emission, title = "Tech Emission in RR", folder=folder)
+    plotIt(xGrid, yGrid, profits, title = "Profits", folder=folder)
+    plotIt(xGrid, yGrid, CLtype, title = "Types", folder=folder)
+    plotIt(xGrid, yGrid, invType, title = "Investment Types", folder=folder)
+
+
 def dataGenLin():
     """
     This function roughly reproduces the result in Huang et al. 
@@ -11,15 +34,7 @@ def dataGenLin():
     mean = np.array([100+i*15 for i in range(11)])
     # devn = np.array([5*i for i in range(21)])
     devn = np.array([i*0.1 for i in range(21)]) # Coefficient of variation
-    meanGrid, devnGrid = np.meshgrid(mean, devn)
-    print(meanGrid.shape)
-    Qrr = np.zeros_like(meanGrid)
-    Qur = np.zeros_like(meanGrid)
-    invUR = np.zeros_like(meanGrid)
-    emission = np.zeros_like(meanGrid)
-    profits = np.zeros_like(meanGrid)
-    CLtype = np.zeros_like(meanGrid)
-    invType = np.zeros_like(meanGrid)
+    meanGrid, devnGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType = makeOutputProtoTypes(mean, devn)
     for i in range(len(devn)):
         for j in range(len(mean)):
             input = {'Low':meanGrid[i,j]-devnGrid[i,j]*meanGrid[i,j], 'High':meanGrid[i,j]+devnGrid[i,j]*meanGrid[i,j]}
@@ -35,15 +50,7 @@ def dataGenLin():
             CLtype[i, j] = lookup[ans['CLtype']["Low"]]*10 + lookup[ans['CLtype']["High"]]
             lookup = {'I':0, 'M':1, 'B':2, 'P':3}
             invType[i,j] = lookup[ans['invType']]
-    np.savetxt("./data1/xx.csv",meanGrid, delimiter=',')
-    np.savetxt("./data1/yy.csv",devnGrid, delimiter=',')
-    plotIt(meanGrid, devnGrid, Qrr, title = "Exp Qty in RR")
-    plotIt(meanGrid, devnGrid, Qur, title = "Exp Qty in UR")
-    plotIt(meanGrid, devnGrid, invUR, title = "Capacity Inv in UR")
-    plotIt(meanGrid, devnGrid, emission, title = "Tech Emission in RR")
-    plotIt(meanGrid, devnGrid, profits, title = "Profits")
-    plotIt(meanGrid, devnGrid, CLtype, title = "Types")
-    plotIt(meanGrid, devnGrid, invType, title = "Investment Types")
+    writedata(meanGrid, devnGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType, folder = './data1/')
 
 def dataGenQuad():
     """
@@ -52,15 +59,7 @@ def dataGenQuad():
     mean = np.array([100+i*15 for i in range(11)])
     # devn = np.array([5*i for i in range(21)])
     devn = np.array([i*0.05 for i in range(21)]) # Coefficient of variation
-    meanGrid, devnGrid = np.meshgrid(mean, devn)
-    print(meanGrid.shape)
-    Qrr = np.zeros_like(meanGrid)
-    Qur = np.zeros_like(meanGrid)
-    invUR = np.zeros_like(meanGrid)
-    emission = np.zeros_like(meanGrid)
-    profits = np.zeros_like(meanGrid)
-    CLtype = np.zeros_like(meanGrid)
-    invType = np.zeros_like(meanGrid)
+    meanGrid, devnGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType = makeOutputProtoTypes(mean, devn)
     for i in range(len(devn)):
         for j in range(len(mean)):
             input = {'Low':meanGrid[i,j]-devnGrid[i,j]*meanGrid[i,j], "Mid": meanGrid[i,j], 'High':meanGrid[i,j]+devnGrid[i,j]*meanGrid[i,j]}
@@ -80,16 +79,7 @@ def dataGenQuad():
             CLtype[i, j] = lookup[ans['CLtype']["Low"]]*100 + lookup[ans['CLtype']["Mid"]]*10 + lookup[ans['CLtype']["High"]]
             lookup = {'I':0, 'M':1, 'B':2, 'P':3}
             invType[i,j] = lookup[ans['invType']]
-
-    np.savetxt("./data1/xx.csv",meanGrid, delimiter=',')
-    np.savetxt("./data1/yy.csv",devnGrid, delimiter=',')
-    plotIt(meanGrid, devnGrid, Qrr, title = "Exp Qty in RR")
-    plotIt(meanGrid, devnGrid, Qur, title = "Exp Qty in UR")
-    plotIt(meanGrid, devnGrid, invUR, title = "Capacity Inv in UR")
-    plotIt(meanGrid, devnGrid, emission, title = "Tech Emission in RR")
-    plotIt(meanGrid, devnGrid, profits, title = "Profits")
-    plotIt(meanGrid, devnGrid, CLtype, title = "Carbon Leakage Types")
-    plotIt(meanGrid, devnGrid, invType, title = "Investment Types")
+    writedata(meanGrid, devnGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType, folder = './data1/')
 
 
 if __name__ == "__main__":
