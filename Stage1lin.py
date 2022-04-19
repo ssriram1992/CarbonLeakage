29ae45107080f2168b017dd66a0b51aecb20291b
Sriram Sankaranarayanan <srirams@iima.ac.in>
2022-04-19 12:44:11 +0530

    Run on Apr 17 2022

--- Stage1lin.py
+++ Stage1lin.py
@@ -3,7 +3,7 @@ import numpy as np
 # import matplotlib.pyplot as plt
 import pandas as pd
 from utils import *
-import Stage1 
+import Stage1
 
 def makeOutputProtoTypes(xGridder, yGridder):
     xGrid, yGrid = np.meshgrid(xGridder, yGridder)
@@ -19,13 +19,13 @@ def makeOutputProtoTypes(xGridder, yGridder):
 def writedata(xGrid, yGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType, folder = './data1/'):
     np.savetxt(folder+"xx.csv",xGrid, delimiter=',')
     np.savetxt(folder+"yy.csv",yGrid, delimiter=',')
-    plotIt(xGrid, yGrid, Qrr, title = "Exp Qty in RR", folder=folder)
-    plotIt(xGrid, yGrid, Qur, title = "Exp Qty in UR", folder=folder)
-    plotIt(xGrid, yGrid, invUR, title = "Capacity Inv in UR", folder=folder)
-    plotIt(xGrid, yGrid, emission, title = "Tech Emission in RR", folder=folder)
-    plotIt(xGrid, yGrid, profits, title = "Profits", folder=folder)
-    plotIt(xGrid, yGrid, CLtype, title = "Types", folder=folder)
-    plotIt(xGrid, yGrid, invType, title = "Investment Types", folder=folder)
+    # plotIt(xGrid, yGrid, Qrr, title = "Exp Qty in RR", folder=folder)
+    # plotIt(xGrid, yGrid, Qur, title = "Exp Qty in UR", folder=folder)
+    # plotIt(xGrid, yGrid, invUR, title = "Capacity Inv in UR", folder=folder)
+    # plotIt(xGrid, yGrid, emission, title = "Tech Emission in RR", folder=folder)
+    # plotIt(xGrid, yGrid, profits, title = "Profits", folder=folder)
+    # plotIt(xGrid, yGrid, CLtype, title = "Types", folder=folder)
+    # plotIt(xGrid, yGrid, invType, title = "Investment Types", folder=folder)
     bigArray = np.array([xGrid, yGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType]).reshape(9,-1).T
     # df = pd.DataFrame(bigArray, columns=['x', 'y', 'Qrr', 'Qur', 'invUR', 'emission', 'profits', 'CLtype', 'invType'])
     np.savetxt(folder+"alldata.csv",bigArray,delimiter=' ')
@@ -35,7 +35,7 @@ def writedata(xGrid, yGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType,
 
 def dataGenLin():
     """
-    This function roughly reproduces the result in Huang et al. 
+    This function roughly reproduces the result in Huang et al.
     """
     mean = np.array([100+i*15 for i in range(11)])
     # devn = np.array([5*i for i in range(21)])
@@ -58,29 +58,24 @@ def dataGenLin():
             invType[i,j] = lookup[ans['invType']]
     writedata(meanGrid, devnGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType, folder = './data1/')
 
-def dataGenQuad(folder='./data1/', bordertax=10):
+def dataGenQuad(folder='./data1/', bordertax=10, upgradeLin = 0, upgradeQuad=20000):
     """
-    This function roughly reproduces the result in Huang et al. 
+    This function roughly reproduces the result in Huang et al.
     """
-    #mean = np.array([100+i*15 for i in range(11)])
-    # mean = np.linspace(100,250,31)
-    mean = np.linspace(0,95,20)
-    # devn = np.array([5*i for i in range(21)])
-    # devn = np.array([i*0.1 for i in range(11)]) # Coefficient of variation
-    devn = np.linspace(0,1,26)
+    mean = np.linspace(0,250,26)
+    devn = np.linspace(0,1,21)
     meanGrid, devnGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType = makeOutputProtoTypes(mean, devn)
     for i in range(len(devn)):
         for j in range(len(mean)):
-            input = {'Low':meanGrid[i,j]-devnGrid[i,j]*meanGrid[i,j], "Mid": meanGrid[i,j], 'High':meanGrid[i,j]+devnGrid[i,j]*meanGrid[i,j]}
+            input = {'Low':meanGrid[i,j]-devnGrid[i,j]*meanGrid[i,j],
+                    "Mid": meanGrid[i,j],
+                    'High':meanGrid[i,j]+devnGrid[i,j]*meanGrid[i,j]}
             stage1 = Stage1.Stage1(DemInt = 200, DemSl = 1, invURcost = 100,
-                costLrrBase = 10, CostLrr_l = 10, CostLrr_q = 0,
-                costQrrBase = 0.1, upgradeLin = 0, upgradeQuad = 20000,
+                costLrrBase = 15, CostLrr_l = 10, CostLrr_q = 0,
+                costQrrBase = 0.1, upgradeLin = upgradeLin, upgradeQuad = upgradeQuad,
                 CTrrs = input, borderTax = bordertax,
                 costLur = 10, costQur = 0.1, CTur = 0)
             ans = stage1.solve()
-            dictPrint(input, end="-----------")
-            dictPrint(ans)
-
             Qrr[i,j] = ans['E-Qrr']
             Qur[i,j] = ans['E-Qur']
             invUR[i,j] = ans['invUR']
@@ -90,15 +85,40 @@ def dataGenQuad(folder='./data1/', bordertax=10):
             CLtype[i, j] = lookup[ans['CLtype']["Low"]]*100 + lookup[ans['CLtype']["Mid"]]*10 + lookup[ans['CLtype']["High"]]
             lookup = {'I':0, 'M':1, 'B':2, 'P':3}
             invType[i,j] = lookup[ans['invType']]
+            input['BT'] = bordertax
+            dictPrint(input, end="-----\t{:.1f}\t------\n\t".format(upgradeLin))
+            dictPrint(ans)
     return writedata(meanGrid, devnGrid, Qrr, Qur, invUR, emission, profits, CLtype, invType, folder = folder)
 
 
 if __name__ == "__main__":
-    for BT in np.linspace(0,100,21):
-        print("***************************************")
-        print("******************{}********************".format(BT))
-        print("***************************************")
-        dataGenQuad (folder='./dat/BT_'+str(BT)+'_', bordertax = BT)
-    # dataGenQuad (folder='./data-BT10/', bordertax = 10)
-    # print(ans) # expQrrval, expQurval, invURval, M.objVal, emission, type
 
+    ugVals = np.linspace(0,2000,21)
+    BTvals = np.linspace(0,100,21)
+
+    for upgradeLin in ugVals:
+        for BT in BTvals:
+            print("***************************************")
+            print("******************{}********************".format(BT))
+            print("***************************************")
+            dataGenQuad (folder='./dat/BT_'+str(BT)+ '_' + str(upgradeLin) +'_', bordertax = BT, upgradeLin=upgradeLin)
+        nCols = 9
+        anss = []
+        for BT in BTvals:
+            t1 = np.genfromtxt('./dat/BT_'+str(BT)+ '_' + str(upgradeLin) +'_alldata.csv')
+            t1len = t1.shape[0]
+            btCol = np.ones((t1len, 1))*BT
+            t2 = np.hstack((btCol, t1))
+            anss.append(t2)
+        final = np.vstack(anss)
+        np.savetxt('dat/Complete_'+str(upgradeLin)+'.csv', final)
+    nCols = 10
+    anss = []
+    for upgradeLin in ugVals:
+        t1 = np.genfromtxt('dat/Complete_'+str(upgradeLin)+'.csv')
+        t1len = t1.shape[0]
+        uLcol = np.ones((t1len, 1))*upgradeLin
+        t2 = np.hstack((uLcol, t1))
+        anss.append(t2)
+    final = np.vstack(anss)
+    np.savetxt('dat/upgradeLinMods_a.csv', final)
